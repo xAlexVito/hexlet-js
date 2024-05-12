1fe667a8297ee097a86598e0c04d437d8fb2019c
Alexandr Vitomskiy <alexxvitomskiy@gmail.com>
2024-05-12 23:10:59 +0300

    index.js from src

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import _ from "lodash";
+
+console.log("Hello, Hexlet!");
+
+console.log(_.last(["one", "two"]));
+
+const fn = (a, b) => {
+  const c = a + b;
+  return c;
+};
+
+export default fn;
